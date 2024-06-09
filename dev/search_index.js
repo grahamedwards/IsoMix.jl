909d320cc7ce7a148e16666b1cf623e04b7a4e84
Documenter.jl <documenter@juliadocs.github.io>
2024-06-09 17:17:09 +0000

    build based on fce3487

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IsoMix","category":"page"},{"location":"#IsoMix","page":"Home","title":"IsoMix","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IsoMix.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IsoMix]","category":"page"}]
+}
