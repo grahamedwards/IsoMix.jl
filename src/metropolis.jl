2dc43b75c24ef2fa3777c17d185578618f7180f2
Graham H. Edwards <grahamhedwards92@gmail.com>
2024-06-16 21:50:04 -0500

    add docs for new metro support functions

--- src/metropolis.jl
+++ src/metropolis.jl
@@ -1,3 +1,10 @@
+"""
+
+    initialguess(p<:Prior)
+
+Returns a [`System`](@ref) instance corresponding to provided [`Prior`](@ref) instance `p`, assigning initial guesses of each component composition using the central tendancy of each [`Datum`](@ref): mean of `Norm`, log-mean of `logNorm`, midpoint of `Unf`, value of `Constant`, and an arbitrary value of 0.5 for `Unconstrained`.
+
+"""
 function initialguess(p::P) where {D<:Data, P<:Prior{D}}
     prior = ()
     @inbounds for i = fieldnames(P)
@@ -9,7 +16,6 @@ function initialguess(p::P) where {D<:Data, P<:Prior{D}}
     end
     return System(prior...)
 end
-
 initialguess(x::Norm) = x.m 
 initialguess(x::logNorm) = x.lm
 initialguess(x::Unf) = (x.b + x.a)/2
@@ -18,8 +24,13 @@ initialguess(::Unconstrained) = 0.5
 
 
 
+"""
 
+    initialjump(p<:Prior)
 
+Returns a [`System`](@ref) instance corresponding to an initial jumping distribution σ for each component's compositions, given provided [`Prior`](@ref) instance `p`. Jump σ are assigned for each [`Datum`](@ref) as follows, the standar deviation (σ) of `Norm`, the log-σ of `logNorm`, ¼ the range of `Unf`, a value of 0 for `Constant`, and an arbitrary value of 0.1 for `Unconstrained`.
+
+"""
 function initialjump(p::P) where {D<:Data, P<:Prior{D}}
     prior = ()
     @inbounds for i = fieldnames(P)
@@ -31,7 +42,6 @@ function initialjump(p::P) where {D<:Data, P<:Prior{D}}
     end
     return System(prior...)
 end
-
 initialjump(x::Norm) = x.s
 initialjump(x::logNorm) = x.ls 
 initialjump(x::Unf) = (x.b - x.a)/4
@@ -39,16 +49,44 @@ intialjump(x::Constant) = 0
 initialjump(::Unconstrained) = 0.1
 
 
-function jump(p::S, j::S ; rng=Random.Xoshiro()) where S<:System
-    si, ci = drawjump(p)
-    v = getfield(getfield(p,si),ci) + getfield(getfield(j,si),ci)*rand(rng)
-    update(p, si, ci, v), (si, ci, v)
+
+"""
+
+    jump(s<:System, j<:System; rng)
+
+Given a guess of `System` values `s` and corresponding jumping distribution scales in `j`, randomly perturbs one component of `s` and returns the new guess as well as a tuple containing the jumped [`Component`](@ref)/endmember (`:A`, `:B`, `:C`), the jumped component composition (e.g. `:x`, `:y`, `:cx`), and the jump value.
+
+# Example
+
+    julia> jumpedsystem, t = jump(System(Component(1,2),Component(3,4)), System(Component(.1,.5),Component(.2,.4)), rng=IsoMix.Random.Xoshiro(1)); 
+
+    julia> jumpedsystem
+    System2{Component1}(Component1(1.0, 2.349413341845734), Component1(3.0, 4.0))
+
+    julia> t
+    (:A, :cx, 0.34941334184573425)
+
+"""
+function jump(p::S, j::S ; rng=Random.Xoshiro()) where {C<:Component, S<:System{C}}
+    si, ci = rand(rng,fieldnames(S)), rand(rng,fieldnames(C))
+    j = getfield(getfield(j,si),ci)*rand(rng)
+    update(p, si, ci, getfield(getfield(p,si),ci) + j), (si, ci, j)
 end
 
-drawjump(::S, rng=Random.Xoshiro()) where {C<:Component, S<:System{C}} = rand(rng,fieldnames(S)), rand(rng,fieldnames(C))
 
+"""
 
+    update(s<:System, si::Symbol, ci::Symbol, v)
 
+Update the `Component` field `ci` of `System` field `si` of `s` with the value `v`.
+
+# Example
+
+    julia> update(System(Component(1,2),Component(3,4)), :B, :x, 12.)
+    System2{Component1}(Component1(1.0, 2.0), Component1(12.0, 4.0))
+
+"""
+update
 function update(s::System3{Component3}, si::Symbol, ci::Symbol, v::Float64)
     X = s.A
     X = ifelse(si==:B,s.B,X)
@@ -92,31 +130,31 @@ function update(s::System3{Component1}, si::Symbol, ci::Symbol, v::Float64)
 end
 
 function update(s::System2{Component3}, si::Symbol, ci::Symbol, v::Float64)
-    X = ifelse(si==:A,s.A,x.B)
+    X = ifelse(si==:A,s.A,s.B)
     X = Component3(ifelse(ci==:x,v,X.x), ifelse(ci==:cx,v,X.cx), ifelse(ci==:y,v,X.y), ifelse(ci==:cy,v,X.cy), ifelse(ci==:z,v,X.z), ifelse(ci==:cz,v,X.cz))
 
-    if si==:A System2(X,B)
-    elseif si==:B System2(A,X)
+    if si==:A System2(X,s.B)
+    elseif si==:B System2(s.A,X)
     else error("System component/endmember must be A or B")
     end
 end
 
 function update(s::System2{Component2}, si::Symbol, ci::Symbol, v::Float64)
-    X = ifelse(si==:A,s.A,x.B)
+    X = ifelse(si==:A,s.A,s.B)
     X = Component2(ifelse(ci==:x,v,X.x), ifelse(ci==:cx,v,X.cx), ifelse(ci==:y,v,X.y), ifelse(ci==:cy,v,X.cy))
 
-    if si==:A System2(X,B)
-    elseif si==:B System2(A,X)
+    if si==:A System2(X,s.B)
+    elseif si==:B System2(s.A,X)
     else error("System component/endmember must be A or B")
     end
 end
 
 function update(s::System2{Component1}, si::Symbol, ci::Symbol, v::Float64)
-    X = ifelse(si==:A,s.A,x.B)
+    X = ifelse(si==:A,s.A,s.B)
     X = Component1(ifelse(ci==:x,v,X.x), ifelse(ci==:cx,v,X.cx))
 
-    if si==:A System2(X,B)
-    elseif si==:B System2(A,X)
+    if si==:A System2(X,s.B)
+    elseif si==:B System2(s.A,X)
     else error("System component/endmember must be A or B")
     end
 end
